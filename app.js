c4f7a78f0390f26db444043f50e1cee022e01a7c
heroes75 <123456789+odin@users.noreply.github.com>
2026-01-28 07:02:38 +0000

    start the stylising

--- app.js
+++ app.js
@@ -19,7 +19,9 @@ const app = express()
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
-
+const assetsPath = path.join(__dirname, 'public')
+app.use(express.static(assetsPath))
+console.log('assetsPath:', assetsPath)
 app.use(express.urlencoded({ extended: true}))
 app.use(session({
     store: new pgSession({
